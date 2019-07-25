6e5be53a995ebac1bd634e57f1c120d574997c92
unknown <tabish.mahfuz1@gmail.com>
2019-07-25 16:42:31 +0530

    Left JOin State Models in Customer List

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -43,7 +43,7 @@ class CustomerController extends Controller
     }
 
     public function viewCustomers(Request $req) {
-    	$customers = Customer::join('state_models', 'state_models.id', '=', 'customers.state_id')
+    	$customers = Customer::leftJoin('state_models', 'state_models.id', '=', 'customers.state_id')
                     ->select('customers.*', 'state_models.name AS state')
                     ->orderBy('id', 'DESC')->get();
     	return view('customer.view_customers', compact('customers'));
