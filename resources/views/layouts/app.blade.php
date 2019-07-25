add1d66b7f2ef7ee007b302eb51b71f99335ad38
unknown <tabish.mahfuz1@gmail.com>
2019-07-25 15:53:12 +0530

    For Heroku

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,21 +18,21 @@
     <!-- Styles -->
     <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
 
-    <link href="{{ secure_asset('custom-libraries/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('custom-libraries/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <!-- <link href="https://fonts.googleapis.com/     css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet"> -->
 
     <!-- Custom styles for this template-->
-    <link href="{{ secure_asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
 </head>
 <body id="page-top">
     <!-- Bootstrap core JavaScript-->
-    <script src="{{ secure_asset('custom-libraries/jquery/jquery.min.js') }}"></script>
-    <script src="{{ secure_asset('custom-libraries/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('custom-libraries/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('custom-libraries/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="{{ secure_asset('custom-libraries/jquery-easing/jquery.easing.min.js') }}"></script>
+    <script src="{{ asset('custom-libraries/jquery-easing/jquery.easing.min.js') }}"></script>
     <!-- Custom scripts for all pages-->
-    <script src="{{ secure_asset('js/sb-admin-2.min.js') }}"></script>
+    <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
     <div id="app">
         <!-- <main class="py-4"> -->
             <div id="wrapper">
