5d0f99d7aece748b62d180c8e909be9243402b3d
unknown <tabish.mahfuz1@gmail.com>
2019-07-25 15:50:25 +0530

    YEAR Support for PostGre

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Blade;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
